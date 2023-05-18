bdd3ee3b175dba093e1a25c79c55fd074304f0ad
Miko-Reach <127021089+Miko-Reach@users.noreply.github.com>
2023-05-18 19:09:27 +0200

    Initial commit First

---
+++ src/main/java/orange/java/pl2/adventurecentral/AdventurecentralApplication.java
@@ -0,0 +1,13 @@
+package orange.java.pl2.adventurecentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventurecentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventurecentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orange/java/pl2/adventurecentral/AdventurecentralApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.java.pl2.adventurecentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurecentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
