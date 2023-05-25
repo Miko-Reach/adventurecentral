ed16ca12d70bc8b5712dc1f482cfd0ec9483bed2
Miko-Reach <127021089+Miko-Reach@users.noreply.github.com>
2023-05-25 20:33:54 +0200

    Commit udało się stworzyć Pierwszą tabelkę z pustymi kolumnami Hotel oraz jej ID ht.
    Stworzona została również obecnie pusta klasa Cities

---
+++ src/main/java/orange/java/pl2/adventurecentral/options/Cities.java
@@ -0,0 +1,4 @@
+package orange.java.pl2.adventurecentral.options;
+
+public class Cities {
+}
--- src/main/java/orange/java/pl2/adventurecentral/options/Hotel.java
+++ src/main/java/orange/java/pl2/adventurecentral/options/Hotel.java
@@ -1,11 +1,57 @@
 package orange.java.pl2.adventurecentral.options;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+//public class Hotel extends Cities {
+
+
 public class Hotel extends Cities {
 
-    private String hotelName;
 
-    private int hotelType;
+//    Hotel¶
+//
+//    nazwa
+//    standard (ilość gwiazdek)
+//    opis
+//    przynależność do miasta (klucz obcy)
 
-    private String hotelDescription;
 
+
+    @Entity
+    @Table(name = "Hotels")
+
+    public class Hotels extends Cities {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+
+        @Column(name = "hotel_name")
+        private String HotelName;
+
+        @Column(name = "hotel_description")
+        private String hotelDescription;
+
+
+        @Column(name = "hotel_rating")
+        private int hotelRating;
+
+
+
+        //ht to Id dla hoteli wygenerowane
+        @jakarta.persistence.Id
+        @GeneratedValue
+        private Long ht;
+
+
+        public void setHt(Long ht) {
+            this.ht = ht;
+        }
+
+        public Long getHt() {
+            return ht;
+        }
+
+    }
 }
