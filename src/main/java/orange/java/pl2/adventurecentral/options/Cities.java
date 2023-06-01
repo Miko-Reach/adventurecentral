2846f70c5cc5759dc034d58f49cfb73f0736c428
patrycja <patrycja0figat@gmail.com>
2023-06-01 17:46:27 +0200

    [changes]

---
+++ src/main/java/orange/java/pl2/adventurecentral/options/Cities.java
@@ -0,0 +1,8 @@
+package orange.java.pl2.adventurecentral.options;
+
+public class Cities extends Countries {
+
+    String cityName;
+
+
+}
---
+++ src/main/java/orange/java/pl2/adventurecentral/options/Main.java
@@ -0,0 +1,10 @@
+package orange.java.pl2.adventurecentral.options;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cities cities = new Cities();
+
+    }
+}
